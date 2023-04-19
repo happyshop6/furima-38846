014bdac71c5740a3709f1fe6c0a8691518d8e090
happyshop6 <kaiseiyuna1222@gmail.com>
2023-04-19 23:04:17 +0900

    Rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,15 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
-  def index 
+  def index
   end
 
   def show
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+    return if user_signed_in?
 
+    redirect_to action: :index
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
     first_name            { '花子' }
     family_name           { '山田' }
     read_first            { 'ハナコ' }

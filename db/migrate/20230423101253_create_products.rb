d3c344a6f2250ee2e848d77e9f88ce90547d6e4d
happyshop6 <kaiseiyuna1222@gmail.com>
2023-04-23 19:43:05 +0900

    productsテーブル作成

--- db/migrate/20230423101253_create_products.rb
+++ db/migrate/20230423101253_create_products.rb
@@ -1,8 +1,18 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
+      t.references :user,               null: false,foreign_key: true
+      t.string  :name,                  null: false
+      t.text    :explanation,           null: false
+      t.integer :category_id,           null: false
+      t.integer :condition_id,          null: false
+      t.integer :pastage_type_id,       null: false
+      t.integer :prefecture_id,         null: false
+      t.integer :preparation_day_id,    null: false
+      t.integer :price,                 null: false
+     
 
-      t.timestamps
+      t.timestamps 
     end
   end
 end

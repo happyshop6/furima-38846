d585c249395d225f1e2f3cff307621316b6ef341
happyshop6 <kaiseiyuna1222@gmail.com>
2023-04-30 14:14:58 +0900

    出品機能の取り消し

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+<<<<<<< HEAD
   root to: 'products#index'
   resources :products do
     resources :orders, only: [:index, :create]
    end
+=======
+  root to: 'items#index'
+>>>>>>> parent of 5b7fdee (テストコードまでの実装)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

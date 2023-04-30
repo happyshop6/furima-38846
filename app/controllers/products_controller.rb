fbe2014972b58c19e796203407540a70bd49f4ab
happyshop6 <kaiseiyuna1222@gmail.com>
2023-04-30 22:34:45 +0900

    コントローラー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :move_to_signed_in, except: [:index, :show]
 
   def index
-    # @product = Product.includes(:user).order('created_at DESC')
+    @product = Product.includes(:user).order('created_at DESC')
   end
 
   def new

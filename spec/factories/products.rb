70dc1a39f156e5c15eed15a8287ba0ff978f72c8
happyshop6 <kaiseiyuna1222@gmail.com>
2023-04-30 14:24:23 +0900

    Revert "出品機能の取り消し"
    This reverts commit ade0e37c25cd3ae9d2f651e7a2e6dd4bce005af2.

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,25 @@
 FactoryBot.define do
   factory :product do
+<<<<<<< Updated upstream
     
   end
 end
+=======
+    association :user
+
+    name { Faker::Name.name }
+    explanation { Faker::Lorem.sentence }
+    category_id { Faker::Number.between(from: 1, to: 10) }
+    condition_id { Faker::Number.between(from: 1, to: 6) }
+    postage_type_id { Faker::Number.between(from: 1, to: 2) }
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    preparation_day_id { Faker::Number.between(from: 1, to: 3) }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
+
+>>>>>>> Stashed changes

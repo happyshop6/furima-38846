02bd1097f410ed23902b8556c1bbe3f1c1690fa5
happyshop6 <kaiseiyuna1222@gmail.com>
2023-05-05 20:52:50 +0900

    商品削除機能訂正３

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
-  before_action :move_to_signed_in, except: [:index, :show, :edit, :destroy]
+  before_action :move_to_signed_in, except: [:index, :show,]
 
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  before_action :move_product, only: [:edit, :destroy]
+
 
   def index
     @products = Product.includes(:user).order('created_at DESC')
@@ -54,5 +57,10 @@ class ProductsController < ApplicationController
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def move_product
+    if @product.user_id != current_user.id
+      redirect_to root_path
   end
 end

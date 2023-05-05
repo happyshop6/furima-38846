c49a44458491de293f9c6fc02057bcbd705a47ce
happyshop6 <kaiseiyuna1222@gmail.com>
2023-05-05 16:45:44 +0900

    編集機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,14 +23,15 @@ class ProductsController < ApplicationController
   end
 
   def edit
-   if @product.user_id == current_user.id
-   else
-     redirect_to root_path
+      @product = Product.find(params[:id])
+      if @product.user_id == current_user.id
+      else
+        redirect_to root_path
    end
   end
 
   def update
-    @product.update(product_params)
+    @product = Product.find(params[:id])
     if @product.valid?
       redirect_to product_path(product_params)
     else

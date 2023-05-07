990d9e0f042f3abd687f2a531cfd388e6c509acc
happyshop6 <kaiseiyuna1222@gmail.com>
2023-05-07 20:10:47 +0900

    ファイル名の修正

---
+++ spec/factories/item_purchase_forms.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :item_purchase_form do
+    name { Faker::Name.name }
+    explanation { Faker::Lorem.sentence }
+    category_id { Faker::Number.between(from: 1, to: 10) }
+    condition_id { Faker::Number.between(from: 1, to: 6) }
+    postage_type_id { Faker::Number.between(from: 1, to: 2) }
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    preparation_day_id { Faker::Number.between(from: 1, to: 3) }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+  end
+end

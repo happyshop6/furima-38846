46c6435c1419bbf726c11a58e34fd3ac2e4f74c2
happyshop6 <kaiseiyuna1222@gmail.com>
2023-05-20 21:52:47 +0900

    バリデーション修正

--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -1,8 +1,4 @@
 class ItemPurchase < ApplicationRecord
-  attr_accessor :token
-  validates :price, presence: true
-  validates :token, presence: true
-  
   belongs_to :user
   belongs_to :product
   has_one :purchase_info
